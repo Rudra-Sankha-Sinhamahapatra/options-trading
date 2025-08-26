565b8a312f4dab2686fb89d822ce12b542042ec6
Rudra-Sankha-Sinhamahapatra <rssmp120@gmail.com>
2025-08-27 01:55:22 +0530

    added backend common

--- packages/backend-common/index.ts
+++ packages/backend-common/index.ts
@@ -1,9 +1,18 @@
+import dotenv from "dotenv"
+dotenv.config()
+
 export const config = {
     dbUrl: process.env.DATABASE_URL,
     server: {
         port: process.env.PORT || "3000",
         nodeenv: process.env.NODE_ENV || "dev",
         jwtSecret: process.env.JWT_SECRET || "secret"
+    },
+    redis:{
+        url: process.env.REDIS_URL || "redis://localhost:6379"
+    },
+    ws:{
+        port: process.env.WS_PORT || 8080
     }
 }
 
